f0bbce3c9310f8b3f3295321aa8e65449813e990
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2023-08-09 00:11:01 +0300

    add simple light box

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,9 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+
+console.log(gallery);
+
 console.log(galleryItems);
