fe988427e47fcac443c8b3ede6f0ce99515ff505
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2023-08-10 17:56:44 +0300

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,39 @@
+
+// import "simplelightbox/dist/simple-lightbox.js";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 const gallery = document.querySelector('.gallery');
 
+gallery.addEventListener('click', handlelClick);
 
-console.log(gallery);
+function handlelClick (e){
+ e.preventDefault();
+}
 
-console.log(galleryItems);
+const listItem = addListItem(galleryItems);
+addItemToGallery(listItem)
+
+function addListItem(arr){
+return arr.map(({preview, original, description})=>`
+<li class="gallery__item" style ="list-style : none;">
+<a class="gallery__link" href="${original}">
+  <img class="gallery__image" data-source="${original}" src="${preview}" alt="${description}">
+</a>
+</li>`).join(''); 
+}
+
+
+
+function addItemToGallery(item){
+return gallery.insertAdjacentHTML('beforeend', item)
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {  
+  captionsData: "alt",
+captionDelay: 250, 
+})
+
