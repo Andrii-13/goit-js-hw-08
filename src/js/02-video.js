a9a0ffcaba1c5cd7b703932594fc4c8a4ad250fa
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2023-08-15 01:19:03 +0300

    finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LS_KEY = 'videoplayer-current-time';
 
+const onPlay = function ({ seconds }) {
+
+    localStorage.setItem(LS_KEY,seconds);
+
+};
+
+player.on('timeupdate', throttle(onPlay,1000));
+player.setCurrentTime(JSON.parse(localStorage.getItem(LS_KEY)||0)); // з underfind дає помилку тому пишемо || 0.
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+const throttle = require("lodash.throttle");
 
+const form = document.querySelector('.feedback-form');
+const LOCAL_KEY = 'feedback-form-state';
+
+const user = JSON.parse(localStorage.getItem(LOCAL_KEY)) ?? {};
+localStorage.setItem(LOCAL_KEY, JSON.stringify(user));
+form.elements.email.value =
+  JSON.parse(localStorage.getItem(LOCAL_KEY)).email ?? '';
+form.elements.message.value =
+  JSON.parse(localStorage.getItem(LOCAL_KEY)).message ?? '';
+
+form.addEventListener('input', throttle(handlerMail, 500));
+form.addEventListener('input', throttle(handlerMes, 500));
+form.addEventListener('submit', handlerSubmit);
+
+function handlerMail(e) {
+  if (e.target.name === 'email') {
+    user.email = e.target.value;
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(user));
+  }
+}
+
+function handlerMes(e) {
+  if (e.target.name === 'message') {
+    user.message = e.target.value;
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(user));
+  }
+}
+
+function handlerSubmit(e) {
+  e.preventDefault();
+  console.log(user);
+  localStorage.removeItem(LOCAL_KEY);
+  form.elements.email.value = '';
+  form.elements.message.value = '';
+}
