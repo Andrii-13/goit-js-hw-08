e0e7f47683662e13f59c52c9b182a263341695f0
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2023-08-15 23:52:59 +0300

    fixed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,39 +1,19 @@
-
-// import "simplelightbox/dist/simple-lightbox.js";
-import "simplelightbox/dist/simple-lightbox.min.css";
 import SimpleLightbox from "simplelightbox";
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from "./gallery-items";
 
-const gallery = document.querySelector('.gallery');
+const galleryEl = document.querySelector('.gallery')
 
-gallery.addEventListener('click', handlelClick);
-
-function handlelClick (e){
- e.preventDefault();
-}
-
-const listItem = addListItem(galleryItems);
-addItemToGallery(listItem)
-
-function addListItem(arr){
-return arr.map(({preview, original, description})=>`
+const item = galleryItems.map(({preview, original, description})=>`
 <li class="gallery__item" style ="list-style : none;">
 <a class="gallery__link" href="${original}">
   <img class="gallery__image" data-source="${original}" src="${preview}" alt="${description}">
 </a>
 </li>`).join(''); 
-}
 
-
-
-function addItemToGallery(item){
-return gallery.insertAdjacentHTML('beforeend', item)
-}
+galleryEl.insertAdjacentHTML('beforeend',item);
 
 let lightbox = new SimpleLightbox('.gallery a', {  
   captionsData: "alt",
 captionDelay: 250, 
 })
-
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-const throttle = require("lodash.throttle");
+const throttle = require('lodash.throttle');
 
 const form = document.querySelector('.feedback-form');
 const LOCAL_KEY = 'feedback-form-state';
@@ -11,27 +11,18 @@ form.elements.message.value =
   JSON.parse(localStorage.getItem(LOCAL_KEY)).message ?? '';
 
 form.addEventListener('input', throttle(handlerMail, 500));
-form.addEventListener('input', throttle(handlerMes, 500));
 form.addEventListener('submit', handlerSubmit);
 
 function handlerMail(e) {
-  if (e.target.name === 'email') {
-    user.email = e.target.value;
-    localStorage.setItem(LOCAL_KEY, JSON.stringify(user));
-  }
-}
-
-function handlerMes(e) {
-  if (e.target.name === 'message') {
-    user.message = e.target.value;
-    localStorage.setItem(LOCAL_KEY, JSON.stringify(user));
-  }
+  user[e.target.name] = e.target.value;
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(user));
 }
 
 function handlerSubmit(e) {
   e.preventDefault();
   console.log(user);
   localStorage.removeItem(LOCAL_KEY);
-  form.elements.email.value = '';
-  form.elements.message.value = '';
+  form.reset();
+  user.message = '';
+  user.email = '';
 }
